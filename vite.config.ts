0b90f0cd808544ab1c8b5ed3352ae115eb5ec79f
coronoro <streicher.tim@googlemail.com>
2023-08-21 21:44:57 +0200

    improve size

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import {defineConfig} from 'vite'
+import svg from 'vite-plugin-svgo';
+import {rollupPluginSpglsl} from 'spglsl';
+// @ts-ignore
+import {advzipPlugin, defaultViteBuildOptions, ectPlugin, roadrollerPlugin} from 'js13k-vite-plugins';
+// @ts-ignore
+import kontra from 'rollup-plugin-kontra';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-    plugins: [],
-    server: {host: '0.0.0.0', port: 8000},
-    clearScreen: false,
-})
+    build: defaultViteBuildOptions,
+    plugins: [
+        svg({
+            multipass: true,
+        }),
+        rollupPluginSpglsl({
+            minify: true,
+            mangle: true,
+        }),
+        roadrollerPlugin(),
+        ectPlugin(),
+        advzipPlugin(),
+        kontra({
+            gameObject: {
+                scale: true,
+                group: true,
+                anchor: true,
+                rotation: true,
+            },
+            sprite: {
+                animation: true,
+            },
+            vector: {
+                normalize: true,
+            },
+            debug: false
+        }),
+
+    ],
+});
