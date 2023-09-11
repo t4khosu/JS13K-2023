81a5b9d1a49fb62122e77bf7464dbaa084899201
Christian Staudte <christian.staudte@tngtech.com>
2023-09-11 23:36:28 +0200

    change cathar perfect text when dies two times

--- src/entities/npcs/catharPerfect.ts
+++ src/entities/npcs/catharPerfect.ts
@@ -12,25 +12,30 @@ class CatharPerfect extends Character{
         "Without consolamentum you must be reborn",
         "Go through the door next to me and re-enter the field of war",
         "Otherwise you may never receive salvation",
+        ],
+        [
+            "Both gods will grant you their help on your way",
+            "Choose wisely what help you accept",
         ]
     ]
 
     endMonolog: string[] = [
-        "I seems like the catholics fled Toulouse seeing their leader die.",
+        "I seems like the catholics fled Toulouse seeing their leader die",
         "With the might bestowed upon me as a Cathar Perfect",
         "I will make a great exception and hold a consolamentum for you",
-        "May you achieve your deserved salvation."
+        "May you achieve your deserved salvation"
     ]
 
     constructor(x: number, y: number, room?: Room) {
         super(x, y, getSpriteById(3), room);
+        this.inbound = false;
     }
 
     talk(){
         if(this.room instanceof WinRoom){
             Game.getInstance().startChat(this.endMonolog)
         }else{
-            Game.getInstance().startChat(this.startMonologs[0])
+            Game.getInstance().startChat(this.startMonologs[Math.min(1, Game.getInstance().deaths)])
         }
     }
 
